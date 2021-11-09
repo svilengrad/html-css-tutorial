94d77346a6f097e1d2b02f7aa90482f470fc4630
Rado Radev <rado@auxnederlandbv.nl>
2021-11-09 16:52:42 +0200

    Shows different functions and capabilities of arrays

--- js/arrays.js
+++ js/arrays.js
@@ -1,9 +1,21 @@
+/*
+ * Array is a data structure
+ * Random access - able to access every element with its index
+ * Able to modify each and every element
+ * Add elements (at the rear, at the front and everywhere else)
+ * Remove (the same as with Add)
+ */
         //  0    1    2
 let arr = [120, 230, 506];
         // 3
-arr.push(1000);
-console.log(arr[0] * arr[1]); // get value by index
-console.log(arr[arr.length-1]); // get the last value 
+arr.push(1000); // adds to the rear of the array
+// console.log(arr[0] * arr[1]); // get value by index
+// console.log(arr[arr.length-1]); // get the last value 
 
 arr[0] = 121; // change the value of the element with index 0
-console.log(arr[0]);
+arr.pop(); // removes last element
+// [ 121, 230, 506 ]
+arr.splice(1, 1); // splice(n, k). Removes k elements starting from index n
+
+// [ 121, 506 ]
+console.log(arr);
