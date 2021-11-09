f0597be06fc9846803e658d618a5cac3b11cbcd9
Rado Radev <rado@auxnederlandbv.nl>
2021-11-09 16:58:55 +0200

    shows nested objects and access to nested elements

--- js/objects.js
+++ js/objects.js
@@ -2,7 +2,12 @@ let objPerson = {
     "name": "Simeon",
     "age": 21,
     "town": "Svilengrad",
-    "country": "Bulgaria"
+    "country": "Bulgaria",
+    "friends": ["Petya", "Georgi"],
+    "favourite_subjects_with_grades": {
+        "Math": [6, 5, 6, 5],
+        "Physics": [4, 6, 5]
+    }
 };
 
-console.log(objPerson["town"]);
+console.log(objPerson["favourite_subjects_with_grades"]["Math"]);
