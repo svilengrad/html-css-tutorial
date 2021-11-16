6f9b40fd65c677ce451c5b5d5b4897939a42930e
Rado Radev <rado@auxnederlandbv.nl>
2021-11-16 16:59:34 +0200

    demonstrate conditions

---
+++ js/conditions.js
@@ -0,0 +1,17 @@
+let a = 10;
+
+if(a < 10) {
+    console.log("option 1");
+    a = 10;
+} else if(a > 10) {
+    console.log("option 2");
+    a = 5;
+}
+
+if(a == 5) {
+    console.log("option 3");
+} else if(a == 6) {
+    console.log("option 4");
+}
+
+console.log("END");
